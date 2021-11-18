66e78667e986c6a46848595f5f1e8cb19cfbd4ee
Ywai51 <anyunas38@gmail.com>
2021-11-18 21:57:32 +0700

    first commit

---
+++ akun/admin.py
@@ -0,0 +1,42 @@
+from django.contrib import admin
+from .models import NewAkun
+from django.contrib.auth.admin import UserAdmin
+
+# Register your models here.
+class AkunAdminConfig(UserAdmin):
+    model = NewAkun
+    search_fields   = ('email','nama','no_hp')
+    ordering        = ('-tanggal_akun',)
+    list_display    = ('email','nama','no_hp','is_staff','last_login')
+    list_filter    = ('is_staff','is_superuser')
+    fieldsets = (
+        (None, {
+            "fields": (
+                'email','nama','no_hp',
+            ),
+        }),
+        ("Personal Info",{
+            "fields": (
+                'last_login','tanggal_akun',
+            )
+        }),
+        ("Permission",{
+            "fields": (
+                'is_active','is_staff','is_superuser',
+            )
+        }),
+        ("Password",{
+            "fields": (
+                'password',
+            )
+        }),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes':('wide',),
+            'fields': ('email','password1','password2','nama','no_hp','is_active','is_staff','is_superuser',)
+        }),
+    )
+
+admin.site.register(NewAkun, AkunAdminConfig)
---
+++ akun/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.db.models.fields import DateTimeField
+from django.utils import timezone
+
+# Create your models here.
+class AkunManager(BaseUserManager):
+
+    def create_superuser(self, email, nama, no_hp, password, **other_fields):
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError('Belum jadi staff!')
+
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError('Belum jadi super user!')
+        
+        return self.create_user(email, nama, no_hp, password, **other_fields)
+
+
+    def create_user(self, email, nama, no_hp, password, **other_fields):
+        if not email:
+            raise ValueError('Email dibutuhkan!')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, nama=nama, no_hp=no_hp, **other_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class NewAkun(AbstractBaseUser,PermissionsMixin):
+
+    email = models.EmailField(verbose_name="email", max_length=254, unique=True)
+    nama = models.CharField(verbose_name="nama", max_length=70)
+    no_hp = models.CharField(verbose_name="nomor handphone", max_length=15, unique=True)
+    tanggal_akun = models.DateTimeField(default=timezone.now)
+
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    objects = AkunManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['nama','no_hp']
+
+    def __str__(self):
+        return self.email
