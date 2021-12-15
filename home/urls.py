423863dc368bfe12eea5390baf00c92befef6523
Ywai51 <anyunas38@gmail.com>
2021-12-15 20:24:31 +0700

    search page done

--- home/urls.py
+++ home/urls.py
@@ -8,4 +8,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("", views.index , name="index"),
     path("snkLayanan", views.snkIndex, name="snk"),
+    path("search", views.searchView, name="search"),
+    
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from mobil.models import mobil
+from motor.models import motor
+from django.db.models import Q
 # Create your views here.
 def index(request):
     context = {
@@ -16,3 +18,20 @@ def snkIndex(request):
         'subheading':"Mohon dibaca dengan seksama ketentuan menyewa menggunakan website ini."
     }
     return render(request, 'home/snkIndex.html',context)
+
+def searchView(request):
+    if request.method == 'GET':
+        search = request.GET.get("search")
+        
+        mbl = mobil.objects.all().filter(Q(nama_mbl__icontains=search) | Q(merk_mbl__icontains=search))
+        mtr = motor.objects.all().filter(Q(nama_mtr__icontains=search) | Q(merk_mtr__icontains=search))
+
+        context = {
+            'title':"Search Page | R2M",
+            'heading':"Halaman Pencarian",
+            'subheading':"Menampilkan Hasil Pencarian untuk ",
+            'listMotor': mtr,
+            'listMobil': mbl,
+            'value' : search
+        }
+        return render(request, 'home/search.html',context)
